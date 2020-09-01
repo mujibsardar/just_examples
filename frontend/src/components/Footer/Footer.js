318b40678f3155fb843f695e6336fe72ea626365
Michael Conour <michaelconour@gmail.com>
2020-08-31 17:37:25 -0700

    continue to work on footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -4,51 +4,38 @@ import "./style.css";
 export default class Footer extends React.Component {
   render() {
     return (
-        
-        <footer className="footer-distributed">
-<div class="container">
-            <div class="row">
-        <div className="footer-left">
-
-            <h3>Just<span> Examples</span></h3>
-
-            <p className="footer-links">
-                <a href="#" className="link-1">Home</a>            
-
-                <a href="#">About</a>
-                
-                <a href="#">Faq</a>
-                
-                <a href="#">Contact</a>
-            </p>
-
-            <p className="footer-company-name">Just Examples ©2020</p>
-        </div>
-
-        <div className="footer-center">          
-            <div>
-                <i className="fa fa-envelope"></i>
-                <p><a href="mailto:support@company.com">support@company.com</a></p>
+        <div id="custom-footer">
+        <div className="custom-footer-wrapper">
+            <div className="media-block">
+                <a className="media-link" href="https://twitter.com/nasclubdebates">
+                    <img src="https://s3.amazonaws.com/files.enjin.com/851662/Footer_Images_Optimised/Twitter-min.png" />
+                </a>
+                <a className="media-link" href="https://www.youtube.com/channel/UCvUrSp0TTR2I05dS4GVdkdA">
+                    <img src="https://s3.amazonaws.com/files.enjin.com/851662/Footer_Images_Optimised/Youtube-min.png" />
+                </a>
+                <a className="media-link" href="https://www.twitch.tv/nasclubdebates">
+                    <img src="https://s3.amazonaws.com/files.enjin.com/851662/Footer_Images_Optimised/Twitch-min.png" />
+                </a>
+                <a className="media-link discord-link" href="https://discord.gg/aGNz5KE">
+                    <img src="https://s3.amazonaws.com/files.enjin.com/851662/Footer_Images_Optimised/Discord-min.png"/>
+                </a>
+            </div>
+            <div className="text">
+                Have any questions or need assistance?
+            </div>
+            <div className="help-block">
+                <a className="help-button" href="/support">
+                    SUPPORT
+                </a>
+                <a className="help-button" href="/STORE">
+                    STORE
+                </a>
+            </div>
+            <div className="copyright-block">
+                <i className="material-icons">copyright</i> 2020 Just Examples<br/>                
             </div>
         </div>
-
-        <div className="footer-right">
-            <p className="footer-company-about">
-                <span>About the company</span>
-                Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-            </p>
-
-            <div className="footer-icons">
-                <a href="#"><i className="fa fa-facebook"></i></a>
-                <a href="#"><i className="fa fa-twitter"></i></a>
-                <a href="#"><i className="fa fa-linkedin"></i></a>
-                <a href="#"><i className="fa fa-github"></i></a>
-            </div>
-        </div>
-        </div>
-        </div>
-
-    </footer>
+    </div>    
     );
   }
 }
