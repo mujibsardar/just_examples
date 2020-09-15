df9efbb5073f13f4c3eb576c1acec127d51832d4
Michael Conour <michaelconour@gmail.com>
2020-09-14 18:10:57 -0700

    push latest changes

--- client/src/components/TabbedCard/CardContent.js
+++ client/src/components/TabbedCard/CardContent.js
@@ -14,7 +14,7 @@ export default function CardContent() {
       </label>
       <input class="tab" id="tab3" name="nav" type="radio" />
       <label for="tab3">
-        <span class="navicon fa fa-fw fa-space-shuttle"></span>
+        <span class="navicon fa fa-fw fa-person"></span>
       </label>
 
       <div class="tab__copy" id="first_who">
